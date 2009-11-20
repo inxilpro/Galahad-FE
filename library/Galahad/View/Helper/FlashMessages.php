0ae3be6e3cd354e98adead21acc22d30671dc95a
Chris Morrell <github@rchy.net>
2009-11-20 12:16:23 -0500

    Added FlashMessenger view helper

---
+++ library/Galahad/View/Helper/FlashMessages.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * This file is part of the Galahad Framework Extension.
+ * 
+ * The Galahad Framework Extension is free software: you can redistribute 
+ * it and/or modify it under the terms of the GNU General Public License 
+ * as published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ * 
+ * The Galahad Framework Extension is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * General Public License for more details.
+ * 
+ * @category  Galahad
+ * @package   Galahad
+ * @copyright Copyright (c) 2009 Chris Morrell <http://cmorrell.com>
+ * @license   GPL <http://www.gnu.org/licenses/>
+ * @version   0.3
+ */
+
+/**
+ * Gets any messages in the FlashMessenger
+ * 
+ * @category   Galahad
+ * @package    Galahad_View
+ * @copyright  Copyright (c) 2009 Chris Morrell <http://cmorrell.com>
+ * @license    GPL <http://www.gnu.org/licenses/>
+ */
+class Galahad_View_Helper_FlashMessages extends Zend_View_Helper_Abstract
+{
+    /**
+     * Gets any flash messages
+     * @return array
+     */
+    public function flashMessages()
+    {
+        return Zend_Controller_Action_HelperBroker::getStaticHelper('FlashMessenger')->getMessages();
+    }
+}
+
+
