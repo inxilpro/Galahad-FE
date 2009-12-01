04de53e017da62ec81fba3a1d9c171abd9bbc44e
Chris Morrell <github@rchy.net>
2009-11-30 22:55:22 -0500

    Added property methods provider and context for generating property getters and setters on models

---
+++ library/Galahad/Tool/Project/Context/ModelPropertyMethods.php
@@ -0,0 +1,281 @@
+<?php
+/**
+ * This file is part of the Galahad Framework Extension.
+ * 
+ * The Galahad Framework Extension is free software: you can redistribute 
+ * it and/or modify it under the terms of the GNU General Public License 
+ * as published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ * 
+ * The Galahad Framework Extension is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * General Public License for more details.
+ * 
+ * @category  Galahad
+ * @package   Galahad_Tool
+ * @copyright Copyright (c) 2009 Chris Morrell <http://cmorrell.com>
+ * @license   GPL <http://www.gnu.org/licenses/>
+ * @version   0.3
+ */
+
+/**
+ * @see Zend_Tool_Project_Context_Interface
+ */
+require_once 'Zend/Tool/Project/Context/Interface.php';
+
+/**
+ * @see Zend_CodeGenerator_Php_Docblock
+ */
+require_once 'Zend/CodeGenerator/Php/Docblock.php';
+
+/**
+ * @see Zend_CodeGenerator_Php_Docblock_Tag_Param
+ */
+require_once 'Zend/CodeGenerator/Php/Docblock/Tag/Param.php';
+
+/**
+ * @see Zend_CodeGenerator_Php_Docblock_Tag_Return
+ */
+require_once 'Zend/CodeGenerator/Php/Docblock/Tag/Return.php';
+
+/**
+ * @see Zend_Reflection_File
+ */
+require_once 'Zend/Reflection/File.php';
+
+/**
+ * @see Zend_Filter_Word_DashToCamelCase
+ */
+require_once 'Zend/Filter/Word/UnderscoreToCamelCase.php';
+
+/**
+ * @see Zend_Filter_Word_DashToCamelCase
+ */
+require_once 'Galahad/Model/Entity.php';
+
+/**
+ * Context for creating a Galahad-style model
+ * 
+ * @category   Galahad
+ * @package    Galahad_Tool
+ * @copyright  Copyright (c) 2009 Chris Morrell <http://cmorrell.com>
+ * @license    GPL <http://www.gnu.org/licenses/>
+ */
+class Galahad_Tool_Project_Context_ModelPropertyMethods implements Zend_Tool_Project_Context_Interface
+{
+    /**
+     * @var Zend_Tool_Project_Profile_Resource
+     */
+    protected $_resource = null;
+    
+    /**
+     * @var Zend_Tool_Project_Profile_Resource
+     */
+    protected $_modelResource = null;
+    
+    /**
+     * @var string
+     */
+    protected $_modelPath = '';
+
+    /**
+     * @var string
+     */
+    protected $_propertyName = null;
+    
+    /**
+     * init()
+     *
+     * @return Galahad_Tool_Project_Context_ModelPropertyMethods
+     */
+    public function init()
+    {
+        $this->_propertyName = $this->_resource->getAttribute('propertyName');
+        
+        $this->_resource->setAppendable(false);
+        $this->_modelResource = $this->_resource->getParentResource();
+        if (!$this->_modelResource->getContext() instanceof Galahad_Tool_Project_Context_ModelFile) {
+            require_once 'Zend/Tool/Project/Context/Exception.php';
+            throw new Zend_Tool_Project_Context_Exception('ModelPropertyMethods must be a sub resource of a ModelFile');
+        }
+        
+        $this->_modelPath = $this->_modelResource->getContext()->getPath();
+        
+        // make the ModelFile node appendable so we can tack on the property methods.
+        $this->_resource->getParentResource()->setAppendable(true);
+        
+        /*
+         * This code block is now commented, its doing to much for init()
+         *
+        if ($this->_modelPath != '' && self::hasModelPropertyMethods($this->_modelPath, $this->_propertyName)) {
+            require_once 'Zend/Tool/Project/Context/Exception.php';
+            throw new Zend_Tool_Project_Context_Exception('An property named ' . $this->_propertyName . 'Property already exists in this model');
+        }
+        */
+        
+        return $this;
+    }
+    
+    /**
+     * getPersistentAttributes
+     *
+     * @return array
+     */
+    public function getPersistentAttributes()
+    {
+        return array(
+			'propertyName' => $this->getPropertyName()
+        );
+    }
+    
+    /**
+     * getName()
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'ModelPropertyMethods';
+    }
+    
+    /**
+     * setResource()
+     *
+     * @param Zend_Tool_Project_Profile_Resource $resource
+     * @return Galahad_Tool_Project_Context_ModelPropertyMethods
+     */
+    public function setResource(Zend_Tool_Project_Profile_Resource $resource)
+    {
+        $this->_resource = $resource;
+        return $this;
+    }
+    
+    /**
+     * setPropertyName()
+     *
+     * @param string $propertyName
+     * @return Galahad_Tool_Project_Context_ModelPropertyMethods
+     */
+    public function setPropertyName($propertyName)
+    {
+        $this->_propertyName = $propertyName;
+        return $this;
+    }
+    
+    /**
+     * getPropertyName()
+     *
+     * @return string
+     */
+    public function getPropertyName()
+    {
+        return $this->_propertyName;
+    }
+    
+    /**
+     * create()
+     *
+     * @return Galahad_Tool_Project_Context_ModelPropertyMethods
+     */
+    public function create()
+    {
+        if (self::createPropertyMethods($this->_modelPath, $this->_propertyName) === false) {
+            require_once 'Zend/Tool/Project/Context/Exception.php';
+            throw new Zend_Tool_Project_Context_Exception(
+                'Could not create property within model ' . $this->_modelPath 
+                . ' with property name ' . $this->_propertyName);
+        }
+        return $this;
+    }
+    
+    /**
+     * delete()
+     *
+     * @return Galahad_Tool_Project_Context_ModelPropertyMethods
+     */
+    public function delete()
+    {
+        // @todo do this
+        return $this;
+    }
+    
+    /**
+     * createPropertyMethod()
+     *
+     * @param string $modelPath
+     * @param string $propertyName
+     * @param string $body
+     * @return true
+     */
+    public static function createPropertyMethods($modelPath, $propertyName)
+    {
+        if (!file_exists($modelPath)) {
+            return false;
+        }
+        
+        $modelCodeGenFile = Zend_CodeGenerator_Php_File::fromReflectedFileName($modelPath, true, true);
+        
+        $filter = new Zend_Filter_Word_UnderscoreToCamelCase();
+        $methodName = $filter->filter($propertyName);
+                
+        $modelCodeGenFile->getClass()->setMethod(array(
+            'name' => 'get' . $methodName,
+            'body' => "\t\treturn \$this->_getPropertyData('{$propertyName}');",
+            'docblock' => array(
+                'shortDescription' => "Gets the '{$propertyName}' property",
+                'tags' => array(
+                    'return' => new Zend_CodeGenerator_Php_Docblock_Tag_Return(array(
+                        'datatype' => 'mixed',
+                    )),
+                ),
+            ),
+        ));
+        
+        $param = strtolower($methodName{0}) . substr($methodName, 1);
+        $modelCodeGenFile->getClass()->setMethod(array(
+            'name' => 'set' . $methodName,
+            'parameters' => array(
+                array('name' => $param),
+            ),
+            'body' => "\t\t\$this->_setPropertyData('$propertyName', \${$param});"
+                    . "\nreturn \$this;",
+            'docblock' => array(
+                'shortDescription' => "Sets the '{$propertyName}' property",
+                'tags' => array(
+                    'param' => new Zend_CodeGenerator_Php_Docblock_Tag_Param(array(
+                        'paramname' => $param,
+                        'datatype' => 'mixed',
+                    )),
+                    'return' => new Zend_CodeGenerator_Php_Docblock_Tag_Return(array(
+                        'datatype' => 'Galahad_Entity',
+                    )),
+                ),
+            ),
+        ));
+        
+        file_put_contents($modelPath, $modelCodeGenFile->generate());
+        return true;
+    }
+    
+    /**
+     * hasModelPropertyMethods()
+     *
+     * @param string $modelPath
+     * @param string $propertyName
+     * @return bool
+     */
+    public static function hasPropertyMethods($modelPath, $propertyName)
+    {
+        if (!file_exists($modelPath)) {
+            return false;
+        }
+        
+        $filter = new Zend_Filter_Word_UnderscoreToCamelCase();
+        $methodName = $filter->filter($propertyName);
+        
+        $modelCodeGenFile = Zend_CodeGenerator_Php_File::fromReflectedFileName($modelPath, true, true);
+        $class = $modelCodeGenFile->getClass();
+        return ($class->hasMethod('get' . $methodName) && $class->hasMethod('set' . $methodName));
+    }
+}
---
+++ library/Galahad/Tool/Project/Provider/ModelProperty.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ * This file is part of the Galahad Framework Extension.
+ * 
+ * The Galahad Framework Extension is free software: you can redistribute 
+ * it and/or modify it under the terms of the GNU General Public License 
+ * as published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ * 
+ * The Galahad Framework Extension is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * General Public License for more details.
+ * 
+ * @category  Galahad
+ * @package   Galahad_Tool
+ * @copyright Copyright (c) 2009 Chris Morrell <http://cmorrell.com>
+ * @license   GPL <http://www.gnu.org/licenses/>
+ * @version   0.3
+ */
+
+/**
+ * @see Zend_Tool_Project_Provider_Abstract
+ */
+require_once 'Galahad/Tool/Project/Provider/Abstract.php';
+
+/**
+ * Provider for model properties
+ * 
+ * @category   Galahad
+ * @package    Galahad_Tool
+ * @copyright  Copyright (c) 2009 Chris Morrell <http://cmorrell.com>
+ * @license    GPL <http://www.gnu.org/licenses/>
+ */
+class Galahad_Tool_Project_Provider_ModelProperty extends Galahad_Tool_Project_Provider_Abstract
+{
+    /**
+     * createResource()
+     *
+     * @param Zend_Tool_Project_Profile $profile
+     * @param string $propertyName
+     * @param string $modelName
+     * @param string $moduleName
+     * @return Zend_Tool_Project_Profile_Resource
+     */
+    public static function createResource(Zend_Tool_Project_Profile $profile, $propertyName, $modelName, $moduleName = null)
+    {
+        if (!is_string($propertyName)) {
+            throw new Zend_Tool_Project_Provider_Exception('Galahad_Tool_Project_Provider_ModelProperty::createResource() expects \"propertyName\" is the name of a property resource to create.');
+        }
+
+        if (!is_string($modelName)) {
+            throw new Zend_Tool_Project_Provider_Exception('Galahad_Tool_Project_Provider_ModelProperty::createResource() expects \"modelName\" is the name of a model resource to create.');
+        }
+
+        $modelFile = self::_getModelFileResource($profile, $modelName, $moduleName);   
+        $propertyMethod = $modelFile->createResource('ModelPropertyMethods', array('propertyName' => $propertyName));
+        return $propertyMethod;
+    }
+
+    /**
+     * hasResource()
+     *
+     * @param Zend_Tool_Project_Profile $profile
+     * @param string $propertyName
+     * @param string $modelName
+     * @param string $moduleName
+     * @return Zend_Tool_Project_Profile_Resource
+     */
+    public static function hasResource(Zend_Tool_Project_Profile $profile, $propertyName, $modelName, $moduleName = null)
+    {
+        if (!is_string($propertyName)) {
+            throw new Zend_Tool_Project_Provider_Exception('Galahad_Tool_Project_Provider_ModelProperty::hasResource() expects \"propertyName\" is the name of a property resource to create.');
+        }
+        
+        if (!is_string($modelName)) {
+            throw new Zend_Tool_Project_Provider_Exception('Galahad_Tool_Project_Provider_ModelProperty::hasResource() expects \"modelName\" is the name of a model resource to create.');
+        }
+        
+        $modelFile = self::_getModelFileResource($profile, $modelName, $moduleName);
+        return (($modelFile->search(array('modelPropertyMethods' => array('propertyName' => $propertyName)))) instanceof Zend_Tool_Project_Profile_Resource);
+    }
+
+    /**
+     * _getModelFileResource()
+     *
+     * @param Zend_Tool_Project_Profile $profile
+     * @param string $modelName
+     * @param string $moduleName
+     * @return Zend_Tool_Project_Profile_Resource
+     */
+    protected static function _getModelFileResource(Zend_Tool_Project_Profile $profile, $modelName, $moduleName = null)
+    {
+        $profileSearchParams = array();
+
+        if (null != $moduleName && is_string($moduleName)) {
+            $profileSearchParams = array('modulesDirectory', 'moduleDirectory' => array('moduleName' => $moduleName));
+        }
+
+        $profileSearchParams[] = 'modelsDirectory';
+        $profileSearchParams['modelFile'] = array('modelName' => $modelName);
+
+        return $profile->search($profileSearchParams);
+    }
+
+    /**
+     * create()
+     *
+     * @param string $name
+     * @param string $modelName
+     * @param string $module
+     */
+    public function create($name, $parentModel, $module = null)
+    {
+        $this->_loadProfile();
+        
+        if (self::hasResource($this->_loadedProfile, $name, $parentModel, $module)) {
+            throw new Zend_Tool_Project_Provider_Exception('This model (' . $parentModel . ') already has an property named (' . $name . ')');
+        }
+        
+        $propertyMethod = self::createResource($this->_loadedProfile, $name, $parentModel, $module);
+        
+        $this->_registry->getResponse()->appendContent(
+            'Creating a property named ' . $name .
+            ' inside model at ' . $propertyMethod->getParentResource()->getContext()->getPath());
+        $propertyMethod->create();
+
+        /*
+        if ($viewIncluded) {
+            $viewResource = Galahad_Tool_Project_Provider_View::createResource($this->_loadedProfile, $name, $modelName, $module);
+
+            if ($this->_registry->getRequest()->isPretend()) {
+                $this->_registry->getResponse()->appendContent(
+                    'Would create a view script for the ' . $name . ' property method at ' . $viewResource->getContext()->getPath()
+                    );
+            } else {
+                $this->_registry->getResponse()->appendContent(
+                    'Creating a view script for the ' . $name . ' property method at ' . $viewResource->getContext()->getPath()
+                    );
+                $viewResource->create();
+            }
+
+        }
+        */
+        
+        $this->_storeProfile();
+    }
+}
+
+
