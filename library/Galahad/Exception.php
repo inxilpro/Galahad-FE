a369bbf6ba6ed36e0c70b71574490859e553b7d9
Chris Morrell <github@rchy.net>
2010-03-16 13:45:59 -0400

    Added base Galahad_Exception

---
+++ library/Galahad/Exception.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of the Galahad Framework Extension.
+ * 
+ * The Galahad Framework Extension is free software: you can redistribute 
+ * it and/or modify it under the terms of the GNU General Public License 
+ * as published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ * 
+ * The Galahad Framework Extension is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * General Public License for more details.
+ * 
+ * @category  Galahad
+ * @package   Galahad_Exception
+ * @copyright Copyright (c) 2010 Chris Morrell <http://cmorrell.com>
+ * @license   GPL <http://www.gnu.org/licenses/>
+ * @version   0.3
+ */
+
+/**
+ * @category   Galahad
+ * @package    Galahad_Exception
+ * @copyright  Copyright (c) 2010 Chris Morrell <http://cmorrell.com>
+ * @license    GPL <http://www.gnu.org/licenses/>
+ */
+class Galahad_Exception extends Zend_Exception
+{
+}
